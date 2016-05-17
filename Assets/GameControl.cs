6346b6d25716bfadf744a7aaf3a20766bc18e9a3
unknown <48513@747S10-07.nebo.local>
2016-05-17 10:30:50 -0600

    Basic UI junk, initial game management

---
+++ Assets/GameControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+using HandState = Hand.HandState;
+
+public class GameControl : MonoBehaviour {
+
+	private Hand playerHand;
+	private Hand otherHand;
+
+	// Use this for initialization
+	void Start () {
+		playerHand = GameObject.Find ("Player_Hand").GetComponent<Hand>();
+		otherHand = GameObject.Find ("Other_Hand").GetComponent<Hand>();
+	}
+
+	public void DoInput (HandState state) {
+		playerHand.SetState (HandState.Shaking);
+		playerHand.SetState(state);
+		if (state == HandState.Rock) {
+
+		} else if (state == HandState.Paper) {
+
+		} else if (state == HandState.Scissors) {
+
+		}
+	}
+	public void DoInput (string state) {
+		DoInput ((HandState)Enum.Parse(HandState, state));
+	}
+}
---
+++ Assets/Hand.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+
+	Animator animator;
+	private HandState _handstate;
+	public HandState CurrentHandState {
+		get {
+			return _handstate;
+		}
+	}
+
+	public enum HandState {
+		Shaking,
+		Rock,
+		Paper,
+		Scissors
+	}
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	public void SetState(HandState state) {
+		ClearAllAnim();
+		_handstate = state;
+		switch (state) {
+		case HandState.Rock: {
+			animator.SetBool("Rock", true);
+			animator.SetTrigger("Shoot");
+			break;
+		}
+		case HandState.Paper: {
+			animator.SetBool("Paper", true);
+			animator.SetTrigger("Shoot");
+			break;
+		}
+		case HandState.Scissors: {
+			animator.SetBool("Scissors", true);
+			animator.SetTrigger("Shoot");
+			break;
+		}
+		default: {
+			animator.SetTrigger("Reset");
+			break;
+		}
+		}
+	}
+	private void ClearAllAnim() {
+		animator.SetBool("Rock", false);
+		animator.SetBool("Paper", false);
+		animator.SetBool("Scissors", false);
+	}
+}
